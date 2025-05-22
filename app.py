8d685a0a5226da3b6358b6d060c50b6344e17c22
wtcov04 <wt.cov@hotmail.com>
2025-05-22 18:23:45 +0100

    Remove app.run() for uWSGI deployment

--- app.py
+++ app.py
@@ -666,7 +666,3 @@ def hashtag_trends():
         last_request_time = current_time
 
     return render_template('hashtag_trend.html', graph_url=graph_url, hashtag=hashtag)
-
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
